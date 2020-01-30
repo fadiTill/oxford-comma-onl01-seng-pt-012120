90aa4a35d0b1d6023d1358d16bb2881757349215
fadimata tillman <fadi.tillman@gmail.com>
2020-01-30 18:10:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,8 @@ def oxford_comma(array)
   if array.length == 2
   return "#{array[0]} and #{array[1]}"
 end
+
+def oxford_comma
+  if array.length == 3
+    return "#{array[0]},{array[1],{array[2]} and }"
+  end 
