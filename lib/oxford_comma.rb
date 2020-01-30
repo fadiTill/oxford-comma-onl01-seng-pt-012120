cf6966f880961a8d77d51ae28afeb47366f5ea3c
fadimata tillman <fadi.tillman@gmail.com>
2020-01-30 17:54:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,6 @@
-
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
 def oxford_comma(array)
-  array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
    array[0]
 end
 
