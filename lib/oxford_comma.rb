00fce17d295ee1e110a7543d42da2a9872c666fd
fadimata tillman <fadi.tillman@gmail.com>
2020-01-30 18:15:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
 
+def oxford_comma(array)
+   array[0]
+end
+
+def oxford_comma(array)
+  if array.length == 2
+  return "#{array[0]} and #{array[1]}"
+elsif array.length > 2
+  array[-1].insert(0 , "and ")
+end
+array.join(", ")
 end
